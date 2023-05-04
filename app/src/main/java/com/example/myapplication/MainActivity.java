eb79a4e5ded2855c8febe045a060209790e36acd
robin <s10242331@connect.np.edu.sg>
2023-05-04 23:21:46 +0800

    ni

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button followButton = findViewById(R.id.button);
+
+        followButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String buttonTextString = followButton.getText().toString();
+                if (buttonTextString.equals("FOLLOW")) {
+                    followButton.setText("UNFOLLOW");
+                }
+                else{
+                    followButton.setText("FOLLOW");
+                }
+            }
+        });
+
     }
 }
