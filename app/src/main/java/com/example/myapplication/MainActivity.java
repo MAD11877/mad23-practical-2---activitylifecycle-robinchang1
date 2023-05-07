20f43b26bdc97d01a868598e42066b703de4283b
robin <s10242331@connect.np.edu.sg>
2023-05-07 12:59:31 +0800

    h

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,17 +15,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        User user = new User("john", "hi", 0, false);
+
         Button followButton = findViewById(R.id.button);
 
         followButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String buttonTextString = followButton.getText().toString();
-                if (buttonTextString.equals("FOLLOW")) {
+                if (user.followed == false) {
+                    user.followed = true;
                     followButton.setText("UNFOLLOW");
+                    Toast toast = Toast.makeText(getApplicationContext(), "FOLLOWED", Toast.LENGTH_SHORT);
+                    toast.show();
                 }
                 else{
+                    user.followed = false;
                     followButton.setText("FOLLOW");
+                    Toast toast = Toast.makeText(getApplicationContext(), "UNFOLLOWED", Toast.LENGTH_SHORT);
+                    toast.show();
                 }
             }
         });
